6d48426e62d72778445e4ebb7d7533473f3a1b77
humaun-kabir <asmhumaunkabir@gmail.com>
2021-08-13 17:49:24 +0600

    session

--- session_concept_clear/login.php
+++ session_concept_clear/login.php
@@ -1,6 +1,19 @@
 <?php
-    $uname = $_REQUEST['username'];
-    $pass = $_REQUEST['password'];
+    session_start();
+    if(!isset($_SESSION['uname'])){
+        if(isset($_REQUEST['username']) || isset($_REQUEST['password'])){
+            $uname = $_REQUEST['username'];
+            $pass = $_REQUEST['password'];
+            $_SESSION['uname'] = $uname;
+            $_SESSION['pass'] = $pass;
+            echo "<script> location.href='welcome.php'</script>";
+    
+        }
+    }else{
+        echo "<script> location.href='welcome.php'</script>";
+    }
+    
+    
     
 ?>
 <!DOCTYPE html>
